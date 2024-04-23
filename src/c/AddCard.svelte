84402a59b519976369745bf397ee4e3bf11833b5
Jacek Korzemski <wrp40.000@wp.pl>
2024-04-23 02:43:08 +0200

    Ok, now the sender form works, API reads data, security will come later

--- src/c/AddCard.svelte
+++ src/c/AddCard.svelte
@@ -1,49 +1,85 @@
 <script>
 	import Shadowdark from "./creators/Shadowdark.svelte";
 	import Box from "./Box.svelte";
+	import { onMount } from "svelte";
 
-	let system = "dnd";
+	let token = "";
+	let mount = false;
+	let system = "shd";
+	let isLoading = false;
+	let error = undefined;
+	let btnClass = isLoading ? "btn loading" : "btn";
 
 	const postCard = async (e) => {
+		isLoading = true;
 		e.preventDefault();
+		const form = document.getElementById("add-card-form");
+		const formData = new FormData(form);
+
+		await fetch(`${import.meta.env.PUBLIC_API}/addCard`, {
+			method: "post",
+			body: formData,
+		})
+			.then((res) => {
+				if (res.ok) {
+					// location.href = "/addCardSuccess";
+					isLoading = false;
+				}
+			})
+			.catch((e) => {
+				error = "Wystpił błąd 😟 Spróbuj ponownie później.";
+				isLoading = false;
+				console.error(e);
+			});
 	};
 
-	// const test = () => {
-	// 	const form = document.getElementById("add-card-form");
-	// 	const formData = new FormData(form);
-	// };
+	onMount(() => {
+		mount = true;
+		token = sessionStorage.getItem("token");
+	});
 </script>
 
-<form on:submit|preventDefault={postCard} id="add-card-form">
-	<Box>
-		<div class="intro-data">
-			<div class="grid-item">
-				<label for="name"> Nazwa karty lub postaci </label>
-				<input name="name" id="name" type="text" />
-			</div>
-			<div class="grid-item">
-				<label for="system"> System </label>
-				<select name="system" bind:value={system}>
-					<option value="dnd">Dungeons and Dragons 5e</option>
-					<option value="bf">Basic Fantasy 3e</option>
-					<option value="shd">Shadowdark</option>
-				</select>
-			</div>
-			<div class="grid-item span-2">
-				<label for="description"> Opis karty postaci </label>
-				<input name="description" id="description" type="text" />
-			</div>
-		</div>
-	</Box>
-	{#if system === "shd"}
-		<h1>Wybrany system: Shadowdark</h1>
-		<Box>
-			<Shadowdark />
-		</Box>
-	{/if}
-	<div class="spacer"></div>
-	<button type="submit" class="btn">Zapisz</button>
-</form>
+{#if mount}
+	<div class="loading-protector">
+		{#if isLoading}
+			<div class="hider"></div>
+		{/if}
+		<form on:submit|preventDefault={postCard} id="add-card-form">
+			<Box>
+				<div class="intro-data">
+					<input type="text" name="token" bind:value={token} style="display: none;" />
+					<div class="grid-item">
+						<label for="name"> Nazwa karty lub postaci </label>
+						<input name="name" id="name" type="text" />
+					</div>
+					<div class="grid-item">
+						<label for="system"> System </label>
+						<select name="system" bind:value={system}>
+							<option value="dnd">Dungeons and Dragons 5e</option>
+							<option value="bf">Basic Fantasy 3e</option>
+							<option value="shd">Shadowdark</option>
+						</select>
+					</div>
+					<div class="grid-item span-2">
+						<label for="description"> Opis karty postaci </label>
+						<input name="description" id="description" type="text" />
+					</div>
+				</div>
+			</Box>
+			{#if system === "shd"}
+				<h1>Wybrany system: Shadowdark</h1>
+				<Box>
+					<Shadowdark />
+				</Box>
+			{/if}
+			<div class="spacer"></div>
+			{#if error}
+				<div class="error-box">{error}</div>
+			{/if}
+			<button type="submit" class={btnClass}>Zapisz</button>
+		</form>
+	</div>
+{/if}
 
 <style lang="scss">
 	.spacer {
@@ -65,6 +101,17 @@
 		}
 		.span-2 {
 			grid-column: span 2;
+		}
+	}
+	.loading-protector {
+		position: relative;
+		.hider {
+			position: absolute;
+			width: 100%;
+			height: calc(100% - 62px);
+			z-index: 99;
+			background: rgba(0, 0, 0, 0.5);
+			border-radius: 20px;
 		}
 	}
 </style>
--- src/c/creators/Shadowdark.svelte
+++ src/c/creators/Shadowdark.svelte
@@ -1,4 +1,10 @@
 <script>
+	import Heart from "../img/heart.png";
+	import Shield from "../img/shield.png";
+	import Circle from "../img/circle.png";
+
+	$: hasSpells = false;
+
 	const modCount = (num) => {
 		return Math.floor((num - 10) / 2);
 	};
@@ -36,16 +42,33 @@
 		<input name="character_name" type="text" />
 	</div>
 	<div class="grid-item">
+		<label for="ancestry">Ancestry</label>
+		<input name="ancestry" type="text" />
+	</div>
+	<div class="grid-item">
+		<label for="class">Class</label>
+		<input name="class" type="text" />
+	</div>
+	<div class="grid-item">
+		<label for="background">Background</label>
+		<input name="background" type="text" />
+	</div>
+</div>
+<div class="grid grid-3">
+	<div class="grid-item star-center">
 		<label for="level">Level</label>
-		<input name="level" type="number" />
+		<img src={Circle.src} alt="heart" />
+		<input name="level" type="number" min="0" max="10" />
 	</div>
-	<div class="grid-item">
-		<label for="character_name">Class</label>
-		<input name="character_name" type="text" />
+	<div class="grid-item heart-center">
+		<label for="hp">Hit Points</label>
+		<img src={Heart.src} alt="heart" />
+		<input type="number" name="hp" />
 	</div>
-	<div class="grid-item">
-		<label for="character_name">Background</label>
-		<input name="character_name" type="text" />
+	<div class="grid-item shield-center">
+		<label for="ac">Armor Class</label>
+		<img src={Shield.src} alt="shield" />
+		<input type="number" name="ac" />
 	</div>
 </div>
 <hr />
@@ -75,8 +98,42 @@
 		<input name="cha" type="number" bind:value={cha} />
 	</div>
 	<div class="grid-item">
-		<button class="btn st" on:click={roll}>Roll</button>
+		<button class="btn st" on:click|preventDefault={roll}>Roll</button>
 	</div>
+</div>
+<div class="grid grid-1">
+	<div class="grid-item">
+		<label for="ancestry-talent">Ancestry talent:</label>
+		<input type="text" name="ancestry-talent" />
+	</div>
+</div>
+<div class="grid grid-1">
+	<div class="grid-item">
+		<label for="class_feats">Class Features and talents</label>
+		<textarea name="class_feats"></textarea>
+	</div>
+</div>
+<div class="grid grid-2">
+	<div class="grid-item">
+		<label for="gear">Gear</label>
+		<textarea name="gear"></textarea>
+	</div>
+	<div class="grid-item">
+		<label for="notes">Notes</label>
+		<textarea name="notes"></textarea>
+	</div>
+</div>
+<div class="grid grid-1">
+	<div class="grid-item flex-row">
+		<input type="checkbox" name="has_spells" bind:checked={hasSpells} />
+		<label for="has_spells">Has spells?</label>
+	</div>
+	{#if hasSpells}
+		<div class="grid-item">
+			<label for="spells">Spells list</label>
+			<textarea name="spells"></textarea>
+		</div>
+	{/if}
 </div>
 
 <style lang="scss">
@@ -86,8 +143,22 @@
 		grid-template-columns: 1fr 1fr 1fr 1fr;
 		grid-template-rows: 1fr;
 		gap: 15px;
+		&.grid-1 {
+			grid-template-columns: 1fr;
+		}
+		&.grid-2 {
+			grid-template-columns: 1fr 1fr;
+		}
+		&.grid-3 {
+			grid-template-columns: 1fr 1fr 1fr;
+		}
 		&.grid-6 {
 			grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+		}
+		input[type="text"],
+		input[type="number"] {
+			width: 100%;
+			max-width: none;
 		}
 	}
 	.grid-item {
@@ -99,5 +170,45 @@
 	}
 	.btn.st {
 		margin-top: 24px;
+	}
+	.flex-row {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		align-items: center;
+		gap: 15px;
+	}
+	.star-center,
+	.heart-center,
+	.shield-center {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		position: relative;
+		padding-bottom: 32px;
+		img {
+			position: absolute;
+			top: 16px;
+			max-width: 112px;
+			max-height: 112px;
+		}
+		input[type="number"] {
+			position: relative;
+			margin-top: 16px;
+			max-width: 54px;
+			max-height: 54px;
+			text-align: center;
+			background: transparent;
+			border: none;
+			box-shadow: none;
+		}
+	}
+	.shield-center {
+		img {
+			top: 24px;
+			max-height: 90px;
+			max-width: 90px;
+		}
 	}
 </style>
